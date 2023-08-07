0b16a8f08d510be3730a5644051a58df486a697b
Shubham Shah <shubhamshahrising@gmail.com>
2023-08-07 21:36:09 +0530

    feat: change database name

--- src/db.ts
+++ src/db.ts
@@ -4,17 +4,17 @@ import { Task } from "./types/core";
 
 export type TaskWithOptionalId = Omit<Task, "id"> & { id?: number };
 
-export class Achievo extends Dexie {
+export class ChronaLog extends Dexie {
   // 'friends' is added by dexie when declaring the stores()
   // We just tell the typing system this is the case
   tasks!: Table<TaskWithOptionalId>;
 
   constructor() {
-    super("Achievo");
+    super("ChronaLog");
     this.version(2).stores({
       tasks: "++id, task, parentId", // Primary key and indexed props
     });
   }
 }
 
-export const db = new Achievo();
+export const db = new ChronaLog();
