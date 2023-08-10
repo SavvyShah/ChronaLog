2a58b9e2d1640d84692fcec0da8a6c14d467ad94
Shubham Shah <shubhamshahrising@gmail.com>
2023-08-10 14:15:14 +0530

    feat: add browser testing support

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   // configure vitest. May give a type mismatch error in your code editor. Ignore it.
-  test: {},
+  test: {
+    browser: {
+      enabled: true,
+      headless: true,
+      name: "chrome",
+    },
+  },
   plugins: [react()],
 });
